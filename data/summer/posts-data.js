759d73e920dc2e93e30fe7a3140eeff5e60d2a00
罗小七 <lwalex@foxmail.com>
2017-03-31 00:47:23 +0800

    wx_chwl

---
+++ data/summer/posts-data.js
@@ -0,0 +1,51 @@
+var local_database = [
+    {
+        title: "花好月圆",
+        avatar: "/images/summer.jpg",
+        collect_img: "/images/icon/chat.png",
+        view_img: "/images/icon/view.png",
+        headImgSrc: "/images/summer.jpg",
+        title:"玉渊潭公园　　邂逅樱花雨",
+        place:"北京市海淀区西三环中路玉渊潭公园",
+        besttime:"3月底、4月初开始至5月中下旬",
+        roadmap:"地铁军事博物馆站（1号线、9号线）、白堆子站（9号线）、公主坟站（1号线、10号线）与玉渊潭公园距离均在500米左右，可步行到达。",
+        detail: "北京玉渊潭公园2016年樱花节已于3月23日拉开了序幕，本届樱花节将持续到4月底。尽管上周末早樱还没有完全盛开，但市民早已按耐不住踏春的脚步，玉渊潭公园已经人山人海了。本周末将是玉渊潭公园赏樱高峰期，预计周末每天又会迎来十万观赏大军，呈现游客爆棚的景象。玉渊潭公园除了樱花外，山桃花也很备受游客亲睐。由于桃花比樱花开得早，很多家长还区分不了哪是樱花、哪是桃花，我还跟他们当了讲解员。大家上周末看到的主要是桃花，樱花只有几株粉色的开始闹春了，其他的都在含苞待放。现在已经满园樱花了，呼唤大家周末去踏春赏樱。除了樱花与桃花，一种叫耧斗花的花儿吸引了无数人的眼球。据悉，耧斗花别名猫爪花，原产自欧洲与北美，这也是博主第一次在玉渊潭公园看到，这也属于盛开最早的盆栽或者培植的花。以下按樱花、山桃花、耧斗花、迎春花等顺序展示玉渊潭公园早春盛开的花儿。",
+        postId: 0
+    },
+    {
+        title: "花好月圆",
+        avatar: "/images/spring.jpg",
+        collect_img: "/images/icon/chat.png",
+        view_img: "/images/icon/view.png",
+        headImgSrc: "/images/summer.jpg",
+        title:"天坛公园　　唯美杏花林+清纯丁香花",
+        detail: "天坛公园的杏林花海是北京五环内最大的一片杏花林，而且还是从清代就遗留下来的，鲜为人知的千年暗香。这里棵棵都是老树，清代种植。开起花来，每一棵都很具规模，树形漂亮。4月初，这里的杏花林区内花苞初放、暗香袭人。",
+        postId: 1
+    },
+    {
+        title: "花好月圆",
+        avatar: "/images/autumn.jpg",
+        collect_img: "/images/icon/chat.png",
+        view_img: "/images/icon/view.png",
+        headImgSrc: "/images/summer.jpg",
+        title:"北海公园　　花香沁心七里香",
+        detail: "七里香又称作木箱、十里香，叶翠花繁，浓香四溢，颜色或白或黄，白花如香雪，黄花似披锦。北海公园的木香架很有名，是利用木香的攀爬性搭建的花亭、花架，在公园中自成一景。木香花比一般春花开得晚，至初夏木香花盛开，花姿影倩，香雪如海，令众多游客驻足享受如沐春风的惬意。伴着阳光和木香的幽香，在北海公园里随意逛逛。",
+        postId: 2
+    },
+    {
+        title: "花好月圆",
+        avatar: "/images/dong.jpg",
+        collect_img: "/images/icon/chat.png",
+        view_img: "/images/icon/view.png",
+        headImgSrc: "/images/summer.jpg",
+        title:"奥林匹克森林公园　　春花节芬芳满园",
+        detail: "奥林匹克森林公园位于北京市中轴线的北端，是北京市内最大的城市生态公园，植被资源十分丰富，素来享有北京市“绿肺”的美誉。今年奥森公园的春花节，在两个月的时间内，向游人展示30余个品种、10万余株的春季花卉。公园内的山桃、山杏、连翘、榆叶梅、玉兰、樱花、海棠、碧桃、丁香、黄刺玫等将相继绽放，花开成海，美不胜收。",
+        postId: 3
+    }
+    
+
+
+]
+module.exports = {
+    postList: local_database
+}//定义一个出口，方便其他页面require
