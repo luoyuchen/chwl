a64fb8fcd3b49ef0c5628c703944b6a89ad1260b
luowei <lwalex@foxmail.com>
2017-04-11 17:52:55 +0800

    优化，增加工具

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //app.js
 App({
-  
+  date:{
+    cacheCity:"",
+    city:""
+  },
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
--- pages/tool/tool.js
+++ pages/tool/tool.js
@@ -1,155 +1,52 @@
-//获取应用实例
-
 var app = getApp();
 Page({
-    data: {
-        userInfo: {},
-        openid: "",
-        question: "",
-        questionArr: [],
-        answerArr: { text: "Hello，我是您的私人助手，需要我为您做些什么呢？", code: 100000 },
-        Arr: [],
-        windowHeight: 600,
-        domHeight: 0,
-        scrollHeight: 0,
-        flag: false,
-        disabled: true,
-        focus:false,
-    },
-    onLoad: function() {
-        var that = this
-            //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo, openid) {
-            //更新数据
-            that.setData({
-                userInfo: userInfo,
-                openid: openid
-            })
-        });
-
-        that.data.Arr.push(that.data.answerArr);
-        that.setData({ Arr: that.data.Arr });
-
-        wx.getSystemInfo({
-            success: function(res) {
-                that.setData({ windowHeight: res.windowHeight });
-            }
+  data:{
+    
+  },
+  datePage:function(){
+        wx.navigateTo({
+          url: '../tool/date/date'
         })
-
-    },
-    askquestion: function(e) {
-        var that = this;
-        var currHeight = 0;
-        var text = e.detail.value;
-        this.setData({
-            question: text
-        });
-        if (text.length) {
-            this.setData({
-                disabled: false
-            })
-        } else {
-            this.setData({
-                disabled: true
-            })
-        }
-        if (!this.data.question) return;
-    },
-    scrollSet: function(e) {
-        if (e.detail.deltaY > 0) { //向上滚动
-        } else { //向下滚动
-            if (this.data.flag) { //如果是输入造成的滚动就set,如果是手动就不作处理
-                this.setData({
-                    scrollHeight: e.detail.scrollHeight,
-                    domHeight: e.detail.scrollHeight,
-                    flag: false
-                })
-            }
-        }
-    },
-    bindfocus: function(e) {
-        this.setData({
-            domHeight: this.data.scrollHeight,
+  },
+  weatherPage:function(){
+        wx.navigateTo({
+          url: '../tool/weather/weather'
         })
-    },
-    submitques: function() {
-        var that = this;
-        var currHeight = 0;
-        var ques = this.data.question;
-        var map = {
-            code: -1, //用来区别q&a
-            text: this.data.question,
-        }
-        that.data.Arr.push(map);
-        if (that.data.scrollHeight == 0) {
-            currHeight = that.data.domHeight + 250;
-        } else {
-            currHeight = that.data.scrollHeight;
-        }
-
-        that.setData({
-            Arr: that.data.Arr,
-            question: "",
-            domHeight: currHeight,
-            flag: true,
-            disabled: true,
-        });
-        setTimeout(function() { //目的是为了触发滚动
-            that.setData({
-                domHeight: currHeight,
-                flag: true,
-            });
-        }, 0)
-
-
-        wx.request({
-            url: 'https://api.h6vr.com/bot_ty/get_from_bot',
-            data: {
-                msg: ques,
-                
-            },
-            method: 'get',
-            
-            success: function(res) {
-                console.log(res)
-                var answer={
-                    text:res.data,
-                    code:100000
-                }
-               
-                    console.log(answer.text);
-                
-                if (answer.code == 100000) {
-                    that.data.Arr.push(answer);
-
-                    if (that.data.scrollHeight == 0) {
-                        currHeight = that.data.domHeight + 200;
-                    } else {
-                        currHeight = that.data.scrollHeight;
-                    }
-
-                    that.setData({
-                        Arr: that.data.Arr,
-                        domHeight: currHeight,
-                        flag: true
-                    });
-                    setTimeout(function() { //目的是为了触发滚动
-                        that.setData({
-                            domHeight: currHeight + 20,
-                            flag: true,
-                        });
-                    }, 0)
-                } else {}
-
-            }
-        })
-    },
-
-    onShareAppMessage: function() {
-        return {
-            title: '【不懂？就问智能助手吧】',
-            desc: '',
-            path: 'pages/tool/tool'
-        }
+  },
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+    
+  },
+  onReady:function(){
+    // 生命周期函数--监听页面初次渲染完成
+    
+  },
+  onShow:function(){
+    // 生命周期函数--监听页面显示
+    
+  },
+  onHide:function(){
+    // 生命周期函数--监听页面隐藏
+    
+  },
+  onUnload:function(){
+    // 生命周期函数--监听页面卸载
+    
+  },
+  onPullDownRefresh: function() {
+    // 页面相关事件处理函数--监听用户下拉动作
+    
+  },
+  onReachBottom: function() {
+    // 页面上拉触底事件的处理函数
+    
+  },
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
     }
+  }
 })
